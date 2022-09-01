14cf65a4bcff3eef3e524c0bf5d2b3f24a561674
Fuzss <mrfusseel@gmail.com>
2022-09-01 12:01:22 +0200

    release project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,7 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog].
 
-## [v4.0.0-1.19.2] - 2022-08-31
+## [v4.0.0-1.19.2] - 2022-09-01
 - Initial release
 
 [Keep a Changelog]: https://keepachangelog.com/en/1.0.0/
--- Forge/src/main/java/fuzs/horseexpert/data/ModRecipeProvider.java
+++ Forge/src/main/java/fuzs/horseexpert/data/ModRecipeProvider.java
@@ -6,7 +6,7 @@ import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.world.item.Items;
-import net.minecraftforge.common.Tags;
+import net.minecraft.world.level.block.Blocks;
 
 import java.util.function.Consumer;
 
@@ -19,10 +19,10 @@ public class ModRecipeProvider extends RecipeProvider {
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> p_176532_) {
         ShapedRecipeBuilder.shaped(ModRegistry.MONOCLE_ITEM.get())
-                .define('P', Tags.Items.GLASS_PANES)
+                .define('#', Blocks.GLASS_PANE)
                 .define('G', Items.GOLD_NUGGET)
                 .pattern(" G ")
-                .pattern("GPG")
+                .pattern("G#G")
                 .pattern(" G ")
                 .unlockedBy("has_gold_nugget", has(Items.GOLD_NUGGET))
                 .save(p_176532_);
